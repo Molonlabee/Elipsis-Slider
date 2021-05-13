2a1e706ca4e78b882e482fda6ece93558f5fb8de
Molonlabee <janayahernandez@yahoo.com>
2021-05-13 02:00:18 -0400

    app.js update slider

--- js/app.js
+++ js/app.js
@@ -9,10 +9,11 @@ let init = () =>{
     // Add animation class to slide
     slides[0].classList.add('jumbo-slider__slide--active');
     links[current-1].classList.add('jumbo-slider__link--active');
+    circles[current-1].classList.add('jumbo-slider__circle--filled');
 
     // Update elipsis and links
     let updateNav = (current) =>{
-        console.log(`update current: ${current}`)
+        // console.log(`update current: ${current}`)
         for(let index = 0; index < slides.length; index++){
             links[index].classList.remove('jumbo-slider__link--active');
             circles[index].classList.remove('jumbo-slider__circle--filled');
@@ -20,24 +21,29 @@ let init = () =>{
         links[current-1].classList.add('jumbo-slider__link--active');
         circles[current-1].classList.add('jumbo-slider__circle--filled');
     }
-    let startSliding = () =>{
+    let startSliding = () => {
         setInterval(()=>{
-            console.log(`current: ${current}`)
+            // console.log(`current: ${current}`)
+
+            // remove active class from first slide, add to second slide, 2nd slide become first slide.
             slides[1].classList.add('jumbo-slider__slide--active');
             slides[0].classList.remove('jumbo-slider__slide--active');
+            // clone first slide and move to last index.
+            container.appendChild(slides[0].cloneNode([true]));
+            // remove first slide after clone.
+            container.removeChild(slides[0]);
 
             console.log(`slides: ${slides.length}`);
             if(current < slides.length){
                 current++
                 updateNav(current)
-            }else{
+            } else {
                 current = 1
+                updateNav(current)
             }
-            container.appendChild(slides[0].cloneNode([true]));
-            container.removeChild(slides[0]);
-        }, 6000);
+           
+        }, time);
     }
     startSliding();
 }   
-
 init();
